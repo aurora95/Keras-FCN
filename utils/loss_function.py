833532cfd4138f4bd1f8a359c8fe23351bfc6d79
aurora95 <yajie_xing@foxmail.com>
2016-10-31 19:43:40 +0800

    fixed the problem that loss might turn to nan.

--- utils/loss_function.py
+++ utils/loss_function.py
@@ -5,12 +5,12 @@ import tensorflow as tf
 # Softmax cross-entropy loss function for segmentation
 def softmax_sparse_crossentropy_ignoring_last_label(labels, x):
     x = K.reshape(x, (-1, K.int_shape(x)[-1]))
-    x = x + K.epsilon()
-    softmax = K.softmax(x)
+    log_softmax = tf.nn.log_softmax(x)
 
     labels = K.one_hot(tf.to_int32(K.flatten(labels)), K.int_shape(x)[-1]+1)
     labels = tf.pack(tf.unpack(labels, axis=-1)[:-1], axis=-1)
 
-    cross_entropy = -K.sum(labels * K.log(softmax), axis=1)
+    cross_entropy = -K.sum(labels * log_softmax, axis=1)
     cross_entropy_mean = K.mean(cross_entropy)
+
     return cross_entropy_mean
