018376e394e3721e0ec44b45b5d9965c085f3a5f
aurora95 <yajie_xing@foxmail.com>
2017-02-18 13:19:55 +0800

    update to fit tf1.0

--- utils/loss_function.py
+++ utils/loss_function.py
@@ -8,8 +8,8 @@ def softmax_sparse_crossentropy_ignoring_last_label(y_true, y_pred):
     log_softmax = tf.nn.log_softmax(y_pred)
 
     y_true = K.one_hot(tf.to_int32(K.flatten(y_true)), K.int_shape(y_pred)[-1]+1)
-    unpacked = tf.unpack(y_true, axis=-1)
-    y_true = tf.pack(unpacked[:-1], axis=-1)
+    unpacked = tf.unstack(y_true, axis=-1)
+    y_true = tf.stack(unpacked[:-1], axis=-1)
 
     cross_entropy = -K.sum(y_true * log_softmax, axis=1)
     cross_entropy_mean = K.mean(cross_entropy)
--- utils/metrics.py
+++ utils/metrics.py
@@ -9,9 +9,9 @@ def sparse_accuracy_ignoring_last_label(y_true, y_pred):
 
     y_true = K.one_hot(tf.to_int32(K.flatten(y_true)),
                        nb_classes + 1)
-    unpacked = tf.unpack(y_true, axis=-1)
+    unpacked = tf.unstack(y_true, axis=-1)
     legal_labels = ~tf.cast(unpacked[-1], tf.bool)
-    y_true = tf.pack(unpacked[:-1], axis=-1)
+    y_true = tf.stack(unpacked[:-1], axis=-1)
 
     return K.sum(tf.to_float(legal_labels & K.equal(K.argmax(y_true, axis=-1), K.argmax(y_pred, axis=-1)))) / K.sum(tf.to_float(legal_labels))
 
