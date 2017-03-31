7443e6a5795052a7661a9a725bc95b9634cfcee8
Andrew Hundt <ATHundt@gmail.com>
2017-03-31 16:48:30 -0400

    batch size 4

--- train.py
+++ train.py
@@ -136,7 +136,7 @@ if __name__ == '__main__':
     # model_name = 'AtrousFCN_Resnet50_16s'
     #model_name = 'Atrous_DenseNet'
     model_name = 'DenseNet_FCN'
-    batch_size = 2
+    batch_size = 4
     batchnorm_momentum = 0.95
     nb_epoch = 450
     lr_base = 0.2 * (float(batch_size) / 4)
