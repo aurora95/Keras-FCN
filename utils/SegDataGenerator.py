6ef581a24eff9cb47ddd4ac5192be17bce50b25b
Andrew Hundt <ATHundt@gmail.com>
2017-04-12 18:12:20 -0400

    MSCOCO initial steps towards support

--- utils/SegDataGenerator.py
+++ utils/SegDataGenerator.py
@@ -6,65 +6,85 @@ import numpy as np
 import os
 import cv2
 
+
 def center_crop(x, center_crop_size, data_format, **kwargs):
-    if data_format=='channels_first':
-        centerw, centerh = x.shape[1]//2, x.shape[2]//2
-    elif data_format=='channels_last':
-        centerw, centerh = x.shape[0]//2, x.shape[1]//2
-    lw, lh = center_crop_size[0]//2, center_crop_size[1]//2
-    rw, rh = center_crop_size[0]-lw, center_crop_size[1]-lh
-    if data_format=='channels_first':
-        return x[:, centerw-lw:centerw+rw, centerh-lh:centerh+rh]
-    elif data_format=='channels_last':
-        return x[centerw-lw:centerw+rw, centerh-lh:centerh+rh, :]
+    if data_format == 'channels_first':
+        centerw, centerh = x.shape[1] // 2, x.shape[2] // 2
+    elif data_format == 'channels_last':
+        centerw, centerh = x.shape[0] // 2, x.shape[1] // 2
+    lw, lh = center_crop_size[0] // 2, center_crop_size[1] // 2
+    rw, rh = center_crop_size[0] - lw, center_crop_size[1] - lh
+    if data_format == 'channels_first':
+        return x[:, centerw - lw:centerw + rw, centerh - lh:centerh + rh]
+    elif data_format == 'channels_last':
+        return x[centerw - lw:centerw + rw, centerh - lh:centerh + rh, :]
+
 
 def pair_center_crop(x, y, center_crop_size, data_format, **kwargs):
-    if data_format=='channels_first':
-        centerw, centerh = x.shape[1]//2, x.shape[2]//2
-    elif data_format=='channels_last':
-        centerw, centerh = x.shape[0]//2, x.shape[1]//2
-    lw, lh = center_crop_size[0]//2, center_crop_size[1]//2
-    rw, rh = center_crop_size[0]-lw, center_crop_size[1]-lh
-    if data_format=='channels_first':
-        return x[:, centerw-lw:centerw+rw, centerh-lh:centerh+rh], y[:, centerw-lw:centerw+rw, centerh-lh:centerh+rh]
-    elif data_format=='channels_last':
-        return x[centerw-lw:centerw+rw, centerh-lh:centerh+rh, :], y[centerw-lw:centerw+rw, centerh-lh:centerh+rh, :]
+    if data_format == 'channels_first':
+        centerw, centerh = x.shape[1] // 2, x.shape[2] // 2
+    elif data_format == 'channels_last':
+        centerw, centerh = x.shape[0] // 2, x.shape[1] // 2
+    lw, lh = center_crop_size[0] // 2, center_crop_size[1] // 2
+    rw, rh = center_crop_size[0] - lw, center_crop_size[1] - lh
+    if data_format == 'channels_first':
+        return x[:, centerw - lw:centerw + rw, centerh - lh:centerh + rh], \
+               y[:, centerw - lw:centerw + rw, centerh - lh:centerh + rh]
+    elif data_format == 'channels_last':
+        return x[centerw - lw:centerw + rw, centerh - lh:centerh + rh, :], \
+               y[centerw - lw:centerw + rw, centerh - lh:centerh + rh, :]
+
 
 def random_crop(x, random_crop_size, data_format, sync_seed=None, **kwargs):
     np.random.seed(sync_seed)
-    if data_format=='channels_first':
+    if data_format == 'channels_first':
         w, h = x.shape[1], x.shape[2]
-    elif data_format=='channels_last':
+    elif data_format == 'channels_last':
         w, h = x.shape[0], x.shape[1]
     rangew = (w - random_crop_size[0]) // 2
     rangeh = (h - random_crop_size[1]) // 2
     offsetw = 0 if rangew == 0 else np.random.randint(rangew)
     offseth = 0 if rangeh == 0 else np.random.randint(rangeh)
-    if data_format=='channels_first':
-        return x[:, offsetw:offsetw+random_crop_size[0], offseth:offseth+random_crop_size[1]]
-    elif data_format=='channels_last':
-        return x[offsetw:offsetw+random_crop_size[0], offseth:offseth+random_crop_size[1], :]
+    if data_format == 'channels_first':
+        return x[:, offsetw:offsetw + random_crop_size[0], offseth:offseth + random_crop_size[1]]
+    elif data_format == 'channels_last':
+        return x[offsetw:offsetw + random_crop_size[0], offseth:offseth + random_crop_size[1], :]
+
 
 def pair_random_crop(x, y, random_crop_size, data_format, sync_seed=None, **kwargs):
     np.random.seed(sync_seed)
-    if data_format=='channels_first':
+    if data_format == 'channels_first':
         w, h = x.shape[1], x.shape[2]
-    elif data_format=='channels_last':
+    elif data_format == 'channels_last':
         w, h = x.shape[0], x.shape[1]
     rangew = (w - random_crop_size[0]) // 2
     rangeh = (h - random_crop_size[1]) // 2
     offsetw = 0 if rangew == 0 else np.random.randint(rangew)
     offseth = 0 if rangeh == 0 else np.random.randint(rangeh)
-    if data_format=='channels_first':
-        return x[:, offsetw:offsetw+random_crop_size[0], offseth:offseth+random_crop_size[1]], y[:, offsetw:offsetw+random_crop_size[0], offseth:offseth+random_crop_size[1]]
-    elif data_format=='channels_last':
-        return x[offsetw:offsetw+random_crop_size[0], offseth:offseth+random_crop_size[1], :], y[offsetw:offsetw+random_crop_size[0], offseth:offseth+random_crop_size[1], :]
+    if data_format == 'channels_first':
+        return x[:, offsetw:offsetw + random_crop_size[0], offseth:offseth + random_crop_size[1]], y[:, offsetw:offsetw + random_crop_size[0], offseth:offseth + random_crop_size[1]]
+    elif data_format == 'channels_last':
+        return x[offsetw:offsetw + random_crop_size[0], offseth:offseth + random_crop_size[1], :], y[offsetw:offsetw + random_crop_size[0], offseth:offseth + random_crop_size[1], :]
+
 
 class SegDirectoryIterator(Iterator):
     '''
     Users need to ensure that all files exist.
     Label images should be png images where pixel values represents class number.
+
+    find images -name *.jpg > images.txt
+    find labels -name *.png > labels.txt
+
+    for a file name 2011_002920.jpg, each row should contain 2011_002920
+
+    file_path: location of train.txt, or val.txt in PASCAL VOC2012 format,
+        listing image file path components without extension
+    data_dir: location of image files referred to by file in file_path
+    label_dir: location of label files
+    data_suffix: image file extension, such as `.jpg` or `.png`
+    label_suffix: label file suffix, such as `.png`, or `.npy`
     '''
+
     def __init__(self, file_path, seg_data_generator,
                  data_dir, data_suffix,
                  label_dir, label_suffix, nb_classes, ignore_label=255,
@@ -77,13 +97,15 @@ class SegDirectoryIterator(Iterator):
             data_format = K.image_data_format()
         self.file_path = file_path
         self.data_dir = data_dir
+        self.data_suffix = data_suffix
+        self.label_suffix = label_suffix
         self.label_dir = label_dir
         self.nb_classes = nb_classes
         self.seg_data_generator = seg_data_generator
         self.target_size = tuple(target_size)
         self.ignore_label = ignore_label
         self.crop_mode = crop_mode
-        self.label_cval=label_cval
+        self.label_cval = label_cval
         self.pad_size = pad_size
         if color_mode not in {'rgb', 'grayscale'}:
             raise ValueError('Invalid color mode:', color_mode,
@@ -107,7 +129,8 @@ class SegDirectoryIterator(Iterator):
             else:
                 self.label_shape = (self.nb_label_ch,) + self.target_size
         elif batch_size != 1:
-            raise ValueError('Batch size must be 1 when target image size is undetermined')
+            raise ValueError(
+                'Batch size must be 1 when target image size is undetermined')
         else:
             self.image_shape = None
             self.label_shape = None
@@ -122,43 +145,58 @@ class SegDirectoryIterator(Iterator):
         self.save_prefix = save_prefix
         self.save_format = save_format
 
-        white_list_formats = {'png', 'jpg', 'jpeg', 'bmp'}
+        white_list_formats = {'png', 'jpg', 'jpeg', 'bmp', 'npy'}
 
-        #build lists for data files and label files
+        # build lists for data files and label files
         self.data_files = []
-        self.label_files= []
+        self.label_files = []
         fp = open(file_path)
         lines = fp.readlines()
-        fp.close( )
+        fp.close()
         self.nb_sample = len(lines)
         for line in lines:
             line = line.strip('\n')
             self.data_files.append(line + data_suffix)
             self.label_files.append(line + label_suffix)
-        super(SegDirectoryIterator, self).__init__(self.nb_sample, batch_size, shuffle, seed)
+        super(SegDirectoryIterator, self).__init__(
+            self.nb_sample, batch_size, shuffle, seed)
 
     def next(self):
         with self.lock:
-            index_array, current_index, current_batch_size = next(self.index_generator)
-        # The transformation of images is not under thread lock so it can be done in parallel
+            index_array, current_index, current_batch_size = next(
+                self.index_generator)
+        # The transformation of images is not under thread lock so it can be
+        # done in parallel
         if self.target_size:
             batch_x = np.zeros((current_batch_size,) + self.image_shape)
-            batch_y = np.zeros((current_batch_size,) + self.label_shape, dtype=int)
+            batch_y = np.zeros((current_batch_size,) +
+                               self.label_shape, dtype=int)
         grayscale = self.color_mode == 'grayscale'
         # build batch of image data and labels
         for i, j in enumerate(index_array):
             data_file = self.data_files[j]
-            label_file= self.label_files[j]
-            img = load_img(os.path.join(self.data_dir, data_file), grayscale=grayscale, target_size=None)
-            label = Image.open(os.path.join(self.label_dir, label_file))
-            if self.save_to_dir and self.palette is None:
-                self.palette = label.palette
+            label_file = self.label_files[j]
+            img_file_format = 'img'
+            img = load_img(os.path.join(self.data_dir, data_file),
+                           grayscale=grayscale, target_size=None)
+            label_filepath = os.path.join(self.label_dir, label_file)
+
+            if self.label_suffix is '.npy' or self.label_suffix is 'npy':
+                y = np.open(label_filepath)
+                label_file_format = 'npy'
+            else:
+                label = Image.open(label_filepath)
+                label_file_format = 'img'
+                if self.save_to_dir and self.palette is None:
+                    self.palette = label.palette
 
             # do padding
             if self.target_size:
                 if self.crop_mode != 'none':
                     x = img_to_array(img, data_format=self.data_format)
-                    y = img_to_array(label, data_format=self.data_format).astype(int)
+                    if label_file_format is not 'npy':
+                        y = img_to_array(
+                            label, data_format=self.data_format).astype(int)
                     img_w, img_h = img.size
                     if self.pad_size:
                         pad_w = max(self.pad_size[1] - img_w, 0)
@@ -167,16 +205,23 @@ class SegDirectoryIterator(Iterator):
                         pad_w = max(self.target_size[1] - img_w, 0)
                         pad_h = max(self.target_size[0] - img_h, 0)
                     if self.data_format == 'channels_first':
-                        x = np.lib.pad(x, ((0, 0), (pad_h/2, pad_h - pad_h/2), (pad_w/2, pad_w - pad_w/2)), 'constant', constant_values=0.)
-                        y = np.lib.pad(y, ((0, 0), (pad_h/2, pad_h - pad_h/2), (pad_w/2, pad_w - pad_w/2)), 'constant', constant_values=self.label_cval)
+                        x = np.lib.pad(x, ((0, 0), (pad_h / 2, pad_h - pad_h / 2), (pad_w / 2, pad_w - pad_w / 2)), 'constant', constant_values=0.)
+                        y = np.lib.pad(y, ((0, 0), (pad_h / 2, pad_h - pad_h / 2), (pad_w / 2, pad_w - pad_w / 2)),
+                                       'constant', constant_values=self.label_cval)
                     elif self.data_format == 'channels_last':
-                        x = np.lib.pad(x, ((pad_h/2, pad_h - pad_h/2), (pad_w/2, pad_w - pad_w/2), (0, 0)), 'constant', constant_values=0.)
-                        y = np.lib.pad(y, ((pad_h/2, pad_h - pad_h/2), (pad_w/2, pad_w - pad_w/2), (0, 0)), 'constant', constant_values=self.label_cval)
+                        x = np.lib.pad(x, ((pad_h / 2, pad_h - pad_h / 2), (pad_w / 2, pad_w - pad_w / 2), (0, 0)), 'constant', constant_values=0.)
+                        y = np.lib.pad(y, ((pad_h / 2, pad_h - pad_h / 2), (pad_w / 2, pad_w - pad_w / 2), (0, 0)), 'constant', constant_values=self.label_cval)
                 else:
-                    x = img_to_array(img.resize((self.target_size[1], self.target_size[0]), Image.BILINEAR), data_format=self.data_format)
-                    y = img_to_array(label.resize((self.target_size[1], self.target_size[0]), Image.NEAREST), data_format=self.data_format).astype(int)
+                    x = img_to_array(img.resize((self.target_size[1], self.target_size[0]),
+                                                Image.BILINEAR),
+                                     data_format=self.data_format)
+                    if label_file_format is not 'npy':
+                        y = img_to_array(label.resize((self.target_size[1], self.target_size[
+                                         0]), Image.NEAREST), data_format=self.data_format).astype(int)
+                    else:
+                        print('ERROR: resize not implemented for label npy file')
 
-            if self.target_size == None:
+            if self.target_size is None:
                 batch_x = np.zeros((current_batch_size,) + x.shape)
                 batch_y = np.zeros((current_batch_size,) + y.shape)
 
@@ -184,7 +229,7 @@ class SegDirectoryIterator(Iterator):
             x = self.seg_data_generator.standardize(x)
 
             if self.ignore_label:
-                y[np.where(y==self.ignore_label)] = self.nb_classes
+                y[np.where(y == self.ignore_label)] = self.nb_classes
 
             batch_x[i] = x
             batch_y[i] = y
@@ -193,20 +238,23 @@ class SegDirectoryIterator(Iterator):
             for i in range(current_batch_size):
                 img = array_to_img(batch_x[i], self.data_format, scale=True)
                 label = batch_y[i][:, :, 0].astype('uint8')
-                label[np.where(label==self.nb_classes)] = self.ignore_label
+                label[np.where(label == self.nb_classes)] = self.ignore_label
                 label = Image.fromarray(label, mode='P')
                 label.palette = self.palette
                 fname = '{prefix}_{index}_{hash}'.format(prefix=self.save_prefix,
-                                                                index=current_index + i,
-                                                                hash=np.random.randint(1e4))
-                img.save(os.path.join(self.save_to_dir, 'img_' + fname + '.{format}'.format(format=self.save_format)))
-                label.save(os.path.join(self.save_to_dir, 'label_' + fname + '.png'))
+                                                         index=current_index + i,
+                                                         hash=np.random.randint(1e4))
+                img.save(os.path.join(self.save_to_dir, 'img_' +
+                                      fname + '.{format}'.format(format=self.save_format)))
+                label.save(os.path.join(self.save_to_dir,
+                                        'label_' + fname + '.png'))
         # return
         batch_x = preprocess_input(batch_x)
         if self.class_mode == 'sparse':
             return batch_x, batch_y
         else:
             return batch_x
+
 
 class SegDataGenerator(object):
 
@@ -215,7 +263,7 @@ class SegDataGenerator(object):
                  samplewise_center=False,
                  featurewise_std_normalization=False,
                  samplewise_std_normalization=False,
-                 channelwise_center = False,
+                 channelwise_center=False,
                  rotation_range=0.,
                  width_shift_range=0.,
                  height_shift_range=0.,
@@ -226,9 +274,9 @@ class SegDataGenerator(object):
                  fill_mode='constant',
                  cval=0.,
                  label_cval=255,
-                 crop_mode = 'none',
-                 crop_size = (0, 0),
-                 pad_size = None,
+                 crop_mode='none',
+                 crop_size=(0, 0),
+                 pad_size=None,
                  horizontal_flip=False,
                  vertical_flip=False,
                  rescale=None,
@@ -318,21 +366,27 @@ class SegDataGenerator(object):
         assert x.shape[img_row_index] == y.shape[img_row_index] and x.shape[img_col_index] == y.shape[
             img_col_index], 'DATA ERROR: Different shape of data and label!\ndata shape: %s, label shape: %s' % (str(x.shape), str(y.shape))
 
-        # use composition of homographies to generate final transform that needs to be applied
+        # use composition of homographies to generate final transform that
+        # needs to be applied
         if self.rotation_range:
-            theta = np.pi / 180 * np.random.uniform(-self.rotation_range, self.rotation_range)
+            theta = np.pi / 180 * \
+                np.random.uniform(-self.rotation_range, self.rotation_range)
         else:
             theta = 0
         rotation_matrix = np.array([[np.cos(theta), -np.sin(theta), 0],
                                     [np.sin(theta), np.cos(theta), 0],
                                     [0, 0, 1]])
         if self.height_shift_range:
-            tx = np.random.uniform(-self.height_shift_range, self.height_shift_range) * crop_size[1]#* x.shape[img_row_index]
+            # * x.shape[img_row_index]
+            tx = np.random.uniform(-self.height_shift_range,
+                                   self.height_shift_range) * crop_size[1]
         else:
             tx = 0
 
         if self.width_shift_range:
-            ty = np.random.uniform(-self.width_shift_range, self.width_shift_range) * crop_size[0] #* x.shape[img_col_index]
+            # * x.shape[img_col_index]
+            ty = np.random.uniform(-self.width_shift_range,
+                                   self.width_shift_range) * crop_size[0]
         else:
             ty = 0
 
@@ -350,17 +404,20 @@ class SegDataGenerator(object):
         if self.zoom_range[0] == 1 and self.zoom_range[1] == 1:
             zx, zy = 1, 1
         else:
-            zx, zy = np.random.uniform(self.zoom_range[0], self.zoom_range[1], 2)
+            zx, zy = np.random.uniform(
+                self.zoom_range[0], self.zoom_range[1], 2)
         if self.zoom_maintain_shape:
             zy = zx
         zoom_matrix = np.array([[zx, 0, 0],
                                 [0, zy, 0],
                                 [0, 0, 1]])
 
-        transform_matrix = np.dot(np.dot(np.dot(rotation_matrix, translation_matrix), shear_matrix), zoom_matrix)
+        transform_matrix = np.dot(
+            np.dot(np.dot(rotation_matrix, translation_matrix), shear_matrix), zoom_matrix)
 
         h, w = x.shape[img_row_index], x.shape[img_col_index]
-        transform_matrix = transform_matrix_offset_center(transform_matrix, h, w)
+        transform_matrix = transform_matrix_offset_center(
+            transform_matrix, h, w)
 
         x = apply_transform(x, transform_matrix, img_channel_index,
                             fill_mode=self.fill_mode, cval=self.cval)
@@ -368,7 +425,8 @@ class SegDataGenerator(object):
                             fill_mode='constant', cval=self.label_cval)
 
         if self.channel_shift_range != 0:
-            x = random_channel_shift(x, self.channel_shift_range, img_channel_index)
+            x = random_channel_shift(
+                x, self.channel_shift_range, img_channel_index)
 
         if self.horizontal_flip:
             if np.random.random() < 0.5:
